10a0567c5df37c4c0d266a34508f7f8ac5894f7a
Janne Kiviluoto <jkiviluoto@iki.fi>
2014-04-24 18:00:50 +0300

    Request landscape orientation

--- app/src/main/java/net/deepmindstate/naamat/app/MainActivity.java
+++ app/src/main/java/net/deepmindstate/naamat/app/MainActivity.java
@@ -6,12 +6,15 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
+import static android.content.pm.ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
+
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setRequestedOrientation(SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
     }
 
